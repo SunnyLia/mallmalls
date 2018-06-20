3e6935fca4740230620b266c3e7a995afafde281
sunnyLia <1326155622@qq.com>
2018-06-20 10:26:29 +0800

    添加首页及公共底部tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,25 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+var app = getApp()
+var data = require("./json.js");
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    isSearch: false,
+    swipData: data.data.swipData,
+    products: data.data.products,
+    shops: data.data.shops,
+    swiperOption: {
+      slidesPerView: 2.5,
+      spaceBetween: 10
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onLoad: function () {
+  },
+  onSearch: function (e) {
+    this.setData({ isSearch: !(this.data.isSearch) })
+  },
+  onSubmit: function (e) {
+    var val = e.detail.value;
+    wx.navigateTo({
+      url: '../index/index'
     })
   }
 })
