1bbb89780731e9fac46ced6ed6509c4b00896779
SunnyLia <1326155622@qq.com>
2018-11-02 14:40:48 +0800

    address
    新增收货地址

---
+++ pages/myAddress/json.js
@@ -0,0 +1,14 @@
+var list = {
+    "addressList": [{
+        "user_name": "张张你大爷",
+        "phone": "13111111111",
+        "province_name": "广东省",
+        "city_name": "深圳",
+        "district_name": "南山区",
+        "is_default": "true",
+    }]
+}
+
+module.exports = {
+    data: list.addressList
+}
