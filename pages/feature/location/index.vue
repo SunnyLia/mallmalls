bddf43b74fd485a3b6e53023536c5b0d3d3a0233
sunnyLia <1326155622@qq.com>
2020-07-19 22:44:40 +0800

    11

--- pages/feature/location/index.vue
+++ pages/feature/location/index.vue
@@ -14,38 +14,70 @@
 				latitude: 39.909,
 				longitude: 116.39742,
 				covers: [{
-					id:1,
+					id: 1,
+					latitude: 40.909,
+					longitude: 116.39742,
+					iconPath: '../../../static/images/location.png',
+				}, {
+					id: 1,
 					latitude: 39.909,
 					longitude: 116.39742,
-					iconPath: '../../../static/images/location.png'
+					iconPath: '../../../static/images/location.png',
 				}]
 			}
 		},
 		methods: {
 
 		},
-		mounted() {
+		mounted() {},
+		methods: {
+			getLocation() {
+				let that = this;
+				uni.getLocation({
+					success: function(res) {
+						that.latitude = res.latitude;
+						that.longitude = res.longitude;
+						that.covers = [{
+							id: 1,
+							latitude: res.latitude,
+							longitude: res.longitude,
+							iconPath: '../../../static/images/location.png',
+						}]
+					},
+					fail: function(err) {
+						uni.showToast({
+							title: err.errMsg,
+							icon: "none"
+						});
+					}
+				});
+			}
 		},
 		onLoad() {
-			uni.authorize({
-			    scope: 'scope.userLocation,scope.userInfo,scope.camera',
-			    success() {
-			        console.log("授权成功")
-			    },
-				fail() {
-				    console.log("授权失败")
-				}
-			})
-			console.log(11111111111111)
-			uni.getLocation({
-			    type: 'wgs84',
-			    success: function (res) {
-			        console.log('当前位置的经度：' + res.longitude);
-			        console.log('当前位置的纬度：' + res.latitude);
-			    },fail: function (res) {
-			        console.log('错误：' + res);
-			    }
-			});
+			// uni.authorize({
+			//     scope: 'scope.userLocation,scope.userInfo,scope.camera',
+			//     success() {
+			//         console.log("授权成功")
+			//     },
+			// 	fail() {
+			// 	    console.log("授权失败")
+			// 	}
+			// })
+
+
+			// uni.chooseLocation({
+			// 	success: (data)=> {
+			// 		console.log(data)
+			// 	}
+			// })
+
+			// uni.openLocation({
+			// 	latitude: this.latitude,
+			// 	longitude: this.longitude,
+			// 	success: function() {
+			// 		console.log('success');
+			// 	}
+			// });
 		}
 	}
 </script>
